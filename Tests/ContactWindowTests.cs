fbeebfb0f17a0dd841db0bd26146683ec5d9652c
Artur Strug <artstrug@gmail.com>
2020-03-05 21:05:42 +0100

    Add fill out contact form test

---
+++ Tests/ContactWindowTests.cs
@@ -0,0 +1,29 @@
+﻿using demoblaze_selenium_csharp.Pages;
+using NUnit.Framework;
+
+namespace demoblaze_selenium_csharp.Tests
+{
+    [TestFixture]
+    public class ContactWindowTests : BaseTest
+    {
+        [Test, Order(1)]
+        public void GivenUserContactFormData_WhenUserFillOutContactFormAndClickSendMessageButton_ThenMessageIsSent()
+        {
+            ContactFormData = new ContactFormData("anowak@poczta.pl", "Anna Nowak", "Sample message");
+            
+            ContactWindow = DemoBlazeHomePage.ClickContactLink();
+            ContactWindow.FillOutContactForm(ContactFormData);
+
+            Assert.That(ContactWindow.IsMessageSentSuccessfully, Is.True);
+        }
+
+        [Test, Order(2)]
+        public void WhenUserClosesContactPage_ThenPageIsClosed()
+        {
+            ContactWindow = DemoBlazeHomePage.ClickContactLink();
+            ContactWindow.ClickCloseWindow();
+
+            Assert.That(ContactWindow.IsWindowClosed(), Is.True);
+        }
+    }
+}
